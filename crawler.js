4bf28c116e12cf87ebcd8b6173dba2723993ac0b
soumdatta81 <soumdatta81@gmail.com>
2023-08-21 21:07:55 +0530

    added html w basic error handling

--- crawler.js
+++ crawler.js
@@ -1,6 +1,34 @@
 const {JSDOM} = require('jsdom')
 
 
+
+async function crawlPage(currentURL){
+    console.log(`actively crawling: ${currentURL}`)
+
+
+    try{
+        const resp = await fetch(currentURL)
+
+        if (resp.status > 399){
+            console.log(`err in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+
+        const contentType = resp.headers.get("content-type")
+
+        if (!contentType.includes("text/html")){
+            console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`)
+            return
+        }
+
+
+        console.log(await resp.text())
+    }catch(err){
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
+    }
+   
+}
+
 function getURLsFromHTML(htmlBody, _baseURL){
     const urls = []
     const dom = new JSDOM(htmlBody)
@@ -30,8 +58,6 @@ function getURLsFromHTML(htmlBody, _baseURL){
     return urls
 }
 
-
-
 function normalizeURL(urlString){
     const urlObj = new URL(urlString)
     const hostPath =  `${urlObj.hostname}${urlObj.pathname}`
@@ -44,5 +70,6 @@ function normalizeURL(urlString){
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+const {crawlPage} = require('./crawler.js')
+
+function main(){
+
+    if (process.argv.length < 3){
+        console.log("no website provided")
+        process.exit(1)
+    }
+    if (process.argv.length > 3){
+        console.log("too many command line args")
+        process.exit(1)
+    }
+
+    const _baseURL = process.argv[2]
+
+    for(const arg of process.argv){
+        console.log(arg)
+    }
+
+    console.log(`starting crawl of ${_baseURL}`)
+
+
+    crawlPage(_baseURL)
+
+
+}
+main()
