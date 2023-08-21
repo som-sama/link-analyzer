b1066c9e49c5835d72453f608923e017bfaac8a9
soumdatta81 <soumdatta81@gmail.com>
2023-08-21 20:17:57 +0530

    get urls

--- crawler.js
+++ crawler.js
@@ -1,3 +1,37 @@
+const {JSDOM} = require('jsdom')
+
+
+function getURLsFromHTML(htmlBody, _baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+
+    for (const linkElement of linkElements){
+        if (linkElement.href.slice(0,1) == '/'){
+            // its relative
+            try{
+                const urlObj = new URL(`${_baseURL}${linkElement.href}`)
+                urls.push(normalizeURL(urlObj.href));
+            } catch(err){
+                console.log(`err w relative url: ${err.message}`)
+            }
+          
+        } else {
+            // its absolute
+            try{
+                const urlObj = new URL(linkElement.href)
+                urls.push(normalizeURL(urlObj.href));
+            } catch(err){
+                console.log(`err w relative url: ${err.message}`)
+            }
+        }
+    
+    }
+    return urls
+}
+
+
+
 function normalizeURL(urlString){
     const urlObj = new URL(urlString)
     const hostPath =  `${urlObj.hostname}${urlObj.pathname}`
@@ -9,5 +43,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
